b2fc93ded896f8afd0e7418685d09b6f3fb2d89b
mahmod <yousry.mohmmed@rocketmail.com>
2023-08-17 01:23:03 +0300

    files added

---
+++ js/script.js
@@ -0,0 +1,101 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const apiKey = 'dd35fdefdadb4c21b3b182557231408'; // Replace with your actual API key
+
+  const searchButton = document.getElementById('searchButton');
+  const locationInput = document.getElementById('locationInput');
+  const weatherContainer = document.getElementById('weatherContainer');
+  const middleInfo = document.querySelector('.middle-info-box');
+  const thaDayAfter = document.querySelector('.thaDayAfter');
+
+  const leftBox = document.getElementById('leftBox');
+
+  locationInput.addEventListener('keyup', function () {
+    const location = locationInput.value.trim();
+    if (location !== '') {
+      fetchWeather(location);
+    }
+  });
+
+  async function fetchWeather(location) {
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
+    
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      console.log(data);
+      
+      if (data.error) {
+        weatherContainer.innerHTML = `<p>${data.error.message}</p>`;
+      } else {
+        const weatherInfo = `
+              <p class="m-0">${data.location.name}</p>
+              <h1 class="m-0">
+                <span class="degree">${data.current.temp_c}°C</span>
+                <span><img src="${data.current.condition.icon}" alt=""></span>
+                <p class="m-0 fs-5">${data.current.condition.text}</p>
+              </h1>
+        `;
+        
+        leftBox.innerHTML = weatherInfo;
+ 
+        const secondDayof = `
+              <p class="m-0">${data.location.name}</p>
+              <h1 class="m-0">
+                <span class="degree">${data.forecast.forecastday[1].day.avgtemp_c}°C</span>
+                <span><img src="${data.forecast.forecastday[1].day.condition.icon}" alt=""></span>
+                <p class="m-0 fs-5">${data.forecast.forecastday[1].day.condition.text}</p>
+              </h1>
+        `;
+
+        middleInfo.innerHTML = secondDayof;
+
+        const thirdDay = `
+              <p class="m-0">${data.location.name}</p>
+              <h1 class="m-0">
+                <span class="degree">${data.forecast.forecastday[2].day.avgtemp_c}°C</span>
+                <span><img src="${data.forecast.forecastday[2].day.condition.icon}" alt=""></span>
+                <p class="m-0 fs-5">${data.forecast.forecastday[2].day.condition.text}</p>
+              </h1>
+        `;
+
+        thaDayAfter.innerHTML = thirdDay;
+        
+      }
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      weatherContainer.innerHTML = '<p>An error occurred while fetching weather data.</p>';
+    }
+  }
+
+  fetchWeather('egypt');
+
+
+  // =========================
+
+  // Get the current date
+  const today = new Date();
+
+  console.log(today);
+
+  // Calculate the date for tomorrow
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+
+  // Calculate the date for the day after tomorrow
+  const dayAfterTomorrow = new Date();
+  dayAfterTomorrow.setDate(today.getDate() + 2);
+
+  // Define an array of day names
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  // Get the day names for today, tomorrow, and the day after tomorrow
+  const todayName = daysOfWeek[today.getDay()];
+  const tomorrowName = daysOfWeek[tomorrow.getDay()];
+  const dayAfterTomorrowName = daysOfWeek[dayAfterTomorrow.getDay()];
+
+  document.querySelector('#today').innerHTML = todayName;
+  document.querySelector('#tommorow').innerHTML = tomorrowName;
+  document.querySelector('#after').innerHTML = dayAfterTomorrowName;
+
+});
